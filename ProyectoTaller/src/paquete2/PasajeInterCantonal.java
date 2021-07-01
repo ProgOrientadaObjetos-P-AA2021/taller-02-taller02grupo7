420bfed9c6d7b3982e26b48c448a258b37a02735
KrissBustamante <krissbustamante25@gmail.com>
2021-07-01 15:54:02 -0500

    Trabajogrupal

--- ProyectoTaller/src/paquete2/PasajeInterCantonal.java
+++ ProyectoTaller/src/paquete2/PasajeInterCantonal.java
@@ -11,7 +11,8 @@ import java.io.Serializable;
  *
  * @author reroes
  */
-public abstract class PasajeInterCantonal implements Serializable{
+public abstract class PasajeInterCantonal implements Serializable {
+
     protected String nombreCliente;
     protected String cedula;
     protected String origen;
@@ -19,65 +20,20 @@ public abstract class PasajeInterCantonal implements Serializable{
     protected double numeroKilometros;
     protected double tarifaBase;
     protected double valorPasaje;
-    public PasajeInterCantonal(String nom, String ced, String orig,String
-            dest, double numKil, double tarifa, double pasaje){
-        nombreCliente= nom;
-        cedula= ced;
-        origen=orig;
-        destino=dest;
-        numeroKilometros=numKil;
-        tarifaBase=tarifa;
+
+    public PasajeInterCantonal(String nom, String ced, String orig, String dest,
+            double numKil, double tarifa, double pasaje) {
+        nombreCliente = nom;
+        cedula = ced;
+        origen = orig;
+
     }
-    public void setNombreCliente(String n){
-        nombreCliente=n;
-    }
-    public String getNombreCliente(){
-    return nombreCliente;
-    }
-    public void setCed(String n){
-        cedula=n;
-    }
-    public String getCed(){
-        return cedula;
-    }
-    public void setOrigen(String n){
-        origen=n;
-    }
-    public String getOrigen(){
-        return origen;
-    }
-    public void setDestino(String n){
-        destino=n;
-    }
-    public String getDestino(){
-        return destino;
-    }
-    public void setNumeroKilometros(double n){
-        numeroKilometros=n;
-    }
-    public double getNumeroKilometros(){
-        return numeroKilometros;
-    }
-    public void setValorTarifaBase(double n){
-        tarifaBase=n;
-    }
-    public double getValorTarifaBase(){
-        return tarifaBase;
-    }
-    public abstract void setPasajeInterCantonal();
-    public double getPasajeIntercantonal(){
-        return valorPasaje;
-    }
+
+    public abstract void PasajeInterCantonal();
 
     @Override
     public String toString() {
-        return String.format("El pasajero: %s%tCi:%s\nOrigen: %s\nDestino: %s\n"
-                + "Numero de kilometros recorridos: %s\nValor tarifa base:"
-                + "%s\nTotal a pagar: %s", getNombreCliente(),getCed(),
-                getOrigen(),getDestino(), getNumeroKilometros(),
-                getValorTarifaBase(),getPasajeIntercantonal());
+        return String.format("Pasaje.");
     }
-    
-    
-    
+
 }
